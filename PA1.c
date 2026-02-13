22a049ab0c50d80aef6fc52795f306e58581b17f
Vincent Pierce <vinhpierce@gmail.com>
2026-02-13 15:50:17 -0800

    not seg faulting but its not the right number

--- PA1.c
+++ PA1.c
@@ -24,15 +24,18 @@ int main()
 
 void* threadWork(void* bounds)
 {
-    uint64_t sum = 0;
+    uint64_t* sum = malloc(sizeof(uint64_t)); 
+    uint64_t localSum = 0;
     uint64_t* pBoundary = (uint64_t*)bounds; // make it a pointer so you can get the upper and lower bounds
-    uint64_t lowerLimit = pBoundary;
+    printf("Array element passed in: %lu\n", pBoundary[0]);
+    uint64_t lowerLimit = *pBoundary;
     uint64_t upperLimit = *(pBoundary++); 
     for(int i = lowerLimit; i < upperLimit; i++)
     {
-        sum += i;
+        localSum += i;
     }
-    pthread_exit(sum);
+    *sum = localSum;
+    pthread_exit((void*)sum);
 }
 void Multithreading(uint64_t N, uint8_t NUM_THREADS)
 {
@@ -43,23 +46,23 @@ void Multithreading(uint64_t N, uint8_t NUM_THREADS)
 
     pthread_t threads[8] = {0}; // TODO use dynamic memory create size NUM_THREADS
     int err = 0;
-    int* ret_val = 0;
-    uint64_t bounds[8*2] = {0}; // TODO use dynamic memory to initialize the array to NUM_THREADS
+    uint64_t* ret_val = 0;
+    uint64_t bounds[100] = {0}; // TODO use dynamic memory to initialize the array to NUM_THREADS
     
     start = clock(); // work begins
 
     for(int i = 0; i < NUM_THREADS; i++) // create a thread for each NUM_THREADS
-
     {
-        bounds[i] = workLoad*i; 
-        err = pthread_create(threads[i], NULL, threadWork, bounds);
+        bounds[i*2] = workLoad*i; 
+        bounds[i*2+1] = workLoad*(i+1); 
+        printf("bounds array at index %d is %lu\n", i, bounds[i]);
+        err = pthread_create(&threads[i], NULL, threadWork, (void*)bounds+i);
         if(err)
         {
             printf("Bad thread\n");
-            return 0;
+            kill(0); // terminate all processes
         }
     }
-    printf("After creating threads\n"); 
     for(int i = 0; i < NUM_THREADS; i++)
     {
         // wait for the threads here in the order you created them
@@ -76,8 +79,17 @@ void Multithreading(uint64_t N, uint8_t NUM_THREADS)
     printf("The total summation of 0 through %lu with %d threads is %lu\n", upperLimit, NUM_THREADS, sum);
     printf("The total time to perform the workload multithreading was %f seconds\n", time_taken);
 
-    return 0;
 }
+
+
+
+
+
+
+
+
+
+
 
 void Baseline(uint64_t N)
 {
@@ -99,5 +111,4 @@ void Baseline(uint64_t N)
     printf("The total summation of 0 through %lu is %lu\n", upperLimit, sum);
     printf("The total time to perform the workload sequentially was %f seconds\n", time_taken);
     
-    return 0;
 }
